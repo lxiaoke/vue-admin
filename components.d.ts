d692d9a829bac08774ad6c582c3c1368f0f4e685
Bruce <lxk2370851439@163.com>
2023-03-15 21:16:39 +0800

    更新头部与侧边栏

--- components.d.ts
+++ components.d.ts
@@ -9,9 +9,14 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ElAffix: typeof import('element-plus/es')['ElAffix']
     ElAside: typeof import('element-plus/es')['ElAside']
+    ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElDropdown: typeof import('element-plus/es')['ElDropdown']
+    ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
+    ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElMain: typeof import('element-plus/es')['ElMain']
@@ -21,9 +26,12 @@ declare module '@vue/runtime-core' {
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    IEpArrowDown: typeof import('~icons/ep/arrow-down')['default']
+    IEpDocument: typeof import('~icons/ep/document')['default']
     IEpHomeFilled: typeof import('~icons/ep/home-filled')['default']
     IEpMenu: typeof import('~icons/ep/menu')['default']
     IEpSetting: typeof import('~icons/ep/setting')['default']
+    IEpUserFilled: typeof import('~icons/ep/user-filled')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,17 @@
       <el-aside width="200px">
         <el-scrollbar>
           <el-menu
-            active-text-color="#ffd04b"
+            active-text-color="#409eff"
             background-color="#545c64"
             default-active="2"
             text-color="#fff"
             class="admin-aside-menu"
           >
+            <el-affix>
+              <el-menu-item index="0" class="admin-logo">
+                <span>通用后台管理系统</span>
+              </el-menu-item>
+            </el-affix>
             <el-sub-menu index="1">
               <template #title>
                 <el-icon><i-ep-home-filled /> </el-icon>
@@ -34,7 +39,7 @@
               <span>Navigator Two</span>
             </el-menu-item>
             <el-menu-item index="3" disabled>
-              <el-icon><document /></el-icon>
+              <el-icon><i-ep-document /></el-icon>
               <span>Navigator Three</span>
             </el-menu-item>
             <el-menu-item index="4">
@@ -45,7 +50,25 @@
         </el-scrollbar>
       </el-aside>
       <el-container>
-        <el-header>Header</el-header>
+        <el-header class="admin-header">
+          <div class="flex-grow"></div>
+          <el-dropdown class="admin-header-user">
+            <span class="admin-header-user-link">
+              <el-avatar>
+                <i-ep-user-filled></i-ep-user-filled>
+              </el-avatar>
+              <span>李晓珂</span>
+              <i-ep-arrow-down />
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item>个人中心</el-dropdown-item>
+                <el-dropdown-item>修改密码</el-dropdown-item>
+                <el-dropdown-item divided>退出登录</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </el-header>
         <el-main>
           <router-view></router-view>
         </el-main>
@@ -63,5 +86,32 @@
 }
 .admin-aside-menu {
   min-height: 100vh;
+}
+.admin-logo {
+  background-color: #333;
+  justify-content: center;
+  font-size: 1.1rem;
+  color: white;
+}
+.admin-header {
+  display: flex;
+  border-bottom: 1px solid #ddd;
+  height: 56px;
+}
+.admin-header-user {
+  cursor: pointer;
+  align-items: center;
+}
+.admin-header-user-link {
+  outline: none;
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
+.admin-header-user-link > *:not(:first-child) {
+  margin-left: 5px;
+}
+.flex-grow {
+  flex-grow: 1;
 }
 </style>
